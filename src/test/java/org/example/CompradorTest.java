5c586b000c34f2d37e60b30522ace537a63e5b08
c4milot <camilotapiacorrea@gmail.com>
2023-06-01 03:59:52 -0400

    Update CompradorTest.java
    Se agregan casos de test a CompradorTest

--- src/test/java/org/example/CompradorTest.java
+++ src/test/java/org/example/CompradorTest.java
@@ -8,29 +8,72 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CompradorTest {
-    private Comprador compradorPrueba;
-    private Expendedor expendedorPrueba = new Expendedor(3);
-    private Moneda monedaPrueba = new Moneda1000();
+
+    private Expendedor expendedorPrueba;
+    private Comprador compradorTest;
     @BeforeEach
     void setUp() throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
-        compradorPrueba = new Comprador(monedaPrueba, Productos.COCA, expendedorPrueba);
+       expendedorPrueba = new Expendedor(2);
     }
 
     @AfterEach
     void tearDown() {
     }
+    @Test
+    @DisplayName("Test compra correcta")
+    public void testCompra() throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
+        Moneda1000 moneda = new Moneda1000();
+        assertNotNull(compradorTest = new Comprador(moneda, Productos.COCA, expendedorPrueba));
+    }
+    @Test
+    @DisplayName("Test dos compra correcta")
+    public void testDosCompras() throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
+        Moneda1000 moneda = new Moneda1000();
+        assertNotNull(compradorTest = new Comprador(moneda, Productos.COCA, expendedorPrueba));
+        assertNotNull(compradorTest = new Comprador(moneda, Productos.COCA, expendedorPrueba));
+    }
+    @Test
+    @DisplayName("Test NoHayProductoException")
+    public void testTresCompras() throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
+        Moneda1000 moneda = new Moneda1000();
+        Exception exception = assertThrows(NoHayProductoException.class, () -> {
+            compradorTest = new Comprador(moneda, Productos.COCA, expendedorPrueba);
+            compradorTest = new Comprador(moneda, Productos.COCA, expendedorPrueba);
+            compradorTest = new Comprador(moneda, Productos.COCA, expendedorPrueba);
+        });
+    }
 
     @Test
+    @DisplayName("Test PagoInsuficienteException")
+    public void testCompraPocoDinero() throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
+        Moneda100 moneda = new Moneda100();
+        Exception exception = assertThrows(PagoInsuficienteException.class, () -> {
+            compradorTest = new Comprador(moneda, Productos.COCA, expendedorPrueba);
+        });
+    }
+
+    @Test
+    @DisplayName("Test PagoIncorrectoException")
+    public void testCompraSinDinero() throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
+        Exception exception = assertThrows(PagoIncorrectoException.class, () -> {
+            compradorTest = new Comprador(null, Productos.COCA, expendedorPrueba);
+        });
+    }
+    @Test
     @DisplayName("Test vuelto")
-    public void testvuelto() {
-        System.out.println("el vuelto es $"+compradorPrueba.cuantoVuelto());
-        assertEquals(500, compradorPrueba.cuantoVuelto());
+    public void testvuelto() throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
+        Moneda1000 moneda = new Moneda1000();
+        compradorTest = new Comprador(moneda, Productos.COCA, expendedorPrueba);
+        assertEquals(500, compradorTest.cuantoVuelto());
+        System.out.println("el vuelto es $"+compradorTest.cuantoVuelto());
     }
 
     @Test
     @DisplayName("Test nombreProducto")
-    public void nombreProducto() {
-        System.out.println("consumio: "+ compradorPrueba.queConsumio());
-        assertEquals("cocacola",compradorPrueba.queConsumio());
+    public void nombreProducto() throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
+        Moneda1000 moneda = new Moneda1000();
+        compradorTest = new Comprador(moneda, Productos.COCA, expendedorPrueba);
+        System.out.println("consumio: "+ compradorTest.queConsumio());
+        assertEquals("cocacola",compradorTest.queConsumio());
     }
 }
