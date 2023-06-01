60f0ff66e335c08187e52760a21a5cab499d4c44
c4milot <camilotapiacorrea@gmail.com>
2023-05-31 23:00:58 -0400

    Update DepositoBTest and DepositoBTest
    actualizacion test de los deposito de bebidas y dulces

--- src/test/java/org/example/DepositoBTest.java
+++ src/test/java/org/example/DepositoBTest.java
@@ -2,14 +2,17 @@ package org.example;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DepositoBTest {
+    private DepositoB depositoBTest;
 
     @BeforeEach
     void setUp() {
+        depositoBTest = new DepositoB();
     }
 
     @AfterEach
@@ -17,10 +20,30 @@ class DepositoBTest {
     }
 
     @Test
-    void addBebida() {
+    @DisplayName("test agregar bebida")
+    public void addBebida() {
+        Bebida bebida = new CocaCola(1);
+        depositoBTest.addBebida(bebida);
+        assertEquals(1, depositoBTest.almB.size());
+        assertEquals(bebida, depositoBTest.almB.get(0));
     }
 
     @Test
-    void getBebida() {
+    @DisplayName("Test sacar bebida")
+    public void sacarBebida() {
+        Bebida bebida = new CocaCola(1);
+        depositoBTest.addBebida(bebida);
+        Bebida bebidaRetirada = depositoBTest.getBebida();
+
+        assertNotNull(bebidaRetirada);
+        assertEquals(0, depositoBTest.almB.size());
+        assertEquals(bebida, bebidaRetirada);
+    }
+
+    @Test
+    @DisplayName("Test deposito vacio")
+    public void testSinBebida() {
+        Bebida bebidaRetirada = depositoBTest.getBebida();
+        assertNull(bebidaRetirada);
     }
 }
--- src/test/java/org/example/DepositoDTest.java
+++ src/test/java/org/example/DepositoDTest.java
@@ -2,14 +2,17 @@ package org.example;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DepositoDTest {
+    private DepositoD depositoDTest;
 
     @BeforeEach
     void setUp() {
+        depositoDTest = new DepositoD();
     }
 
     @AfterEach
@@ -17,10 +20,29 @@ class DepositoDTest {
     }
 
     @Test
-    void addDulce() {
+    @DisplayName("test agregar dulce")
+    public void addDulce() {
+        Dulce dulce = new Super8(1);
+        depositoDTest.addDulce(dulce);
+        assertEquals(1, depositoDTest.almD.size());
+        assertEquals(dulce, depositoDTest.almD.get(0));
     }
 
     @Test
-    void getDulce() {
+    @DisplayName("Test sacar dulce")
+    public void sacarDulce() {
+        Dulce dulce = new Super8(1);
+        depositoDTest.addDulce(dulce);
+        Dulce dulceRetirado = depositoDTest.getDulce();
+
+        assertNotNull(dulceRetirado);
+        assertEquals(0, depositoDTest.almD.size());
+        assertEquals(dulce, dulceRetirado);
+    }
+    @Test
+    @DisplayName("Test deposito vacio")
+    public void testSinDulce() {
+        Dulce dulceRetirado = depositoDTest.getDulce();
+        assertNull(dulceRetirado);
     }
 }
