8031454f3c2b0ba40ba82fcc86b2f4bcd126c355
c4milot <camilotapiacorrea@gmail.com>
2023-05-31 21:54:35 -0400

    Update DepositoMTest.java

--- src/test/java/org/example/DepositoMTest.java
+++ src/test/java/org/example/DepositoMTest.java
@@ -2,14 +2,17 @@ package org.example;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DepositoMTest {
+    private DepositoM depositoMTest;
 
     @BeforeEach
     void setUp() {
+        depositoMTest = new DepositoM();
     }
 
     @AfterEach
@@ -17,10 +20,31 @@ class DepositoMTest {
     }
 
     @Test
-    void addMoneda() {
+    @DisplayName("test agregar moneda")
+    public void addMoneda() {
+        Moneda moned = new Moneda500();
+        depositoMTest.addMoneda(moned);
+        assertEquals(1, depositoMTest.almM.size());
+        assertEquals(moned, depositoMTest.almM.get(0));
     }
 
     @Test
-    void getMoneda() {
+    @DisplayName("Test sacar moneda")
+    public void sacarMoneda() {
+        Moneda moned = new Moneda500();
+        depositoMTest.addMoneda(moned);
+        Moneda monedaRetirada = depositoMTest.getMoneda();
+
+        assertNotNull(monedaRetirada);
+        assertEquals(0, depositoMTest.almM.size());
+        assertEquals(moned, monedaRetirada);
     }
+
+    @Test
+    @DisplayName("Test deposito vacio")
+    public void testSinMoneda() {
+        Moneda monedaRetirada = depositoMTest.getMoneda();
+        assertNull(monedaRetirada);
+    }
+
 }
